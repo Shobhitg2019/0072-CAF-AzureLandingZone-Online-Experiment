5a5f1e441325f6f8e1b0ba21175e94e35327b4f8
Preston K. Parsard <autocloudarc@outlook.com>
2021-12-15 12:37:07 -0500

    dependency(storage): specify dependency

--- env/dev/mgt/data.tf
+++ env/dev/mgt/data.tf
@@ -8,4 +8,7 @@ data "azurerm_key_vault_secret" "vm_pw" {
 data "azurerm_storage_account" "sta" {
   name                     = "1${var.resource_codes.storage}${local.rnd_string}"
   resource_group_name      = azurerm_resource_group.rgp.name
+  depends_on = [
+    azurerm_storage_account.sta
+  ]
 }
